5014f8c962f1ababc4f0c31ea932a0654ef9eef0
Stan Verhoeve <s_verhoeve@hotmail.nl>
2026-04-08 15:32:15 +0200

    Updated clipboard for ssh use

--- nvim/.config/nvim/lua/core/clipboard.lua
+++ nvim/.config/nvim/lua/core/clipboard.lua
@@ -1,16 +1,18 @@
 --[[
   Clipboard
   Uses OSC52 to sync clipboard over SSH to local machine.
-  Requires a modern terminal emulator (e.g. Alacritty).
+  Falls back to system clipboard when not on SSH.
 --]]
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+if vim.env.SSH_CONNECTION ~= nil then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
